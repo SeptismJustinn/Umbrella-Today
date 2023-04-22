df9a5eddc0db09bd61a8120646d8d282b02675e8
Tan Justinn <ohhoehoe@gmail.com>
2023-04-22 17:31:21 +0800

    HourlyForecast properly interprets data
    -HourlyForecast now properly translates data into more human-readable information.

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -2,6 +2,29 @@ import React from "react";
 import styles from "./HourlyForecast.module.css";
 
 function HourlyForecast(props) {
+  const precipitationRates = [
+    "Negligible",
+    "0-0.25",
+    "1-4",
+    "4-10",
+    "10-16",
+    "16-30",
+    "30-50",
+    "50-75",
+    ">75",
+  ];
+  const cloudiness = [
+    ,
+    "0%-6%",
+    "6%-19%",
+    "19%-31%",
+    "31%-44%",
+    "44%-56%",
+    "56%-69%",
+    "69%-81%",
+    "81%-94%",
+    "94%-100%",
+  ];
   function decipherWeather(datasetString) {
     console.log(datasetString);
     let string = datasetString;
@@ -64,13 +87,15 @@ function HourlyForecast(props) {
         {props.time < 12 ? props.time + "AM" : (props.time % 12) + "PM"}
       </div>
       <div className="col-md-3">{decipherWeather(props.forecast)}</div>
-      <div className="col-md-2">{props.prec}</div>
+      <div className="col-md-2">
+        {precipitationRates[props.prec] + (props.prec === 0 ? "" : "mm/hr")}
+      </div>
       <div className="col-md-2">
         {props.temp}
         <span>&#8451;</span>
       </div>
       <div className="col-md-2">{props.humidity}</div>
-      <div className="col-md-2">{props.cloudcover}</div>
+      <div className="col-md-2">{cloudiness[props.cloudcover]}</div>
     </div>
   );
 }
--- src/pages/Forecast.jsx
+++ src/pages/Forecast.jsx
@@ -19,7 +19,7 @@ function Forecast() {
           <div className="row">
             <div className="col-md-1">Time</div>
             <div className="col-md-3">Forecast</div>
-            <div className="col-md-2">Precipitation</div>
+            <div className="col-md-2">Average Rainfall</div>
             <div className="col-md-2">Temperature</div>
             <div className="col-md-2">Humidity</div>
             <div className="col-md-2">Cloudiness</div>
@@ -42,7 +42,13 @@ function Forecast() {
           })}
         </div>
       </div>
-      ;
+      <div className={styles.forecastText}>
+        Each forecast is made over a 3-hour time period and takes into account
+        several factors to decide weather. <br />
+        Negligible rainfall does not rule out possible rainfall entirely. When
+        average rainfall is less than 4mm/hr, light showers may still be
+        expected, especially when cloudiness is greater than 20%. <br />
+      </div>
     </>
   );
 }
