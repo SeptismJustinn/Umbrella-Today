f4544dffff3832706c9041f3d3547de60c64bbb1
Tan Justinn <ohhoehoe@gmail.com>
2023-04-24 14:44:57 +0800

    Added contingencies for outdated database
    -If error with database (primarily if dataset happens to be outdated), display error image and message isntead.
    -Added sample data for clear/rainy weathers.

--- src/components/Umbrella.jsx
+++ src/components/Umbrella.jsx
@@ -2,20 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function Umbrella(props) {
+  function displayImage() {
+    if (props.outdated) {
+      return <img src="/umbrella_error.png" alt="Outdated dataset" />;
+    } else {
+      if (props.raining) {
+        return <img src="/umbrella_open.png" />;
+      } else {
+        return <img src="/umbrella_closed.png" />;
+      }
+    }
+  }
+
+  function displayMessage() {
+    if (props.outdated) {
+      return "Problems with database... Bring your umbrella just in case!";
+    } else {
+      if (props.raining) {
+        return "Bring your umbrella along!";
+      } else {
+        return "Leave your umbrella behind!";
+      }
+    }
+  }
+
   return (
     <div className="container">
       <NavLink to="/forecast" state={{ data: props.data, date: props.date }}>
-        {props.raining ? (
-          <img src="/umbrella_open.png" />
-        ) : (
-          <img src="/umbrella_closed.png" alt="" />
-        )}
+        {displayImage()}
       </NavLink>
-      <div className="row">
-        {props.raining
-          ? "Bring your umbrella along!"
-          : "Leave your umbrella behind!"}
-      </div>
+      <div className="row">{displayMessage()}</div>
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,18 +10,19 @@ function Main() {
   const [data, setData] = useState([]);
   // State to contain the date at which data was initialized.
   const [date, setDate] = useState(new Date());
+  // State to check if dataset might be outdated.
+  const [outdated, setOutdated] = useState(false);
 
   // GET method specific to 7timer API.
   async function getData() {
     try {
       // 7timer's init is in UTC time.
-      // const res = await fetch("/weather-test-data-timer.json");
-      const res = await fetch(
-        "https://www.7timer.info/bin/api.pl?lon=103.8&lat=1.4&product=civil&output=json"
-      );
+      const res = await fetch("/weather-test-rain.json");
+      // const res = await fetch(
+      //   "https://www.7timer.info/bin/api.pl?lon=103.8&lat=1.4&product=civil&output=json"
+      // );
       if (res.status === 200) {
         const weatherData = await res.json();
-
         // Clean data:
         const currDate = new Date(
           weatherData.init.slice(0, 4) +
@@ -35,7 +36,13 @@ function Main() {
         );
         setDate(currDate);
         setData(weatherData.dataseries.slice(0, 8));
+        if (new Date().getDate() - currDate.getDate() > 1) {
+          setOutdated(true);
+        } else {
+          setOutdated(false);
+        }
       } else {
+        setOutdated(true);
         throw new Error();
       }
     } catch (err) {
@@ -46,14 +53,26 @@ function Main() {
   // Function to check if there will be rain in the next ~12hrs from rounded up hour at which component rendered.
   function checkRain() {
     const currTime = new Date();
-    const currHour = currTime.getHours() + (currTime.getMinutes() > 30 ? 1 : 0);
+    let currHour = 17;
+    // let currHour = currTime.getHours() + (currTime.getMinutes() > 30 ? 1 : 0);
+    if (currTime.getDate() > date.getDate()) {
+      currHour += 24 - date.getHours();
+      console.log("Next day");
+    }
     const timePoint = Math.floor((currHour - date.getHours()) / 3);
+    console.log(timePoint);
+    console.log(data);
     const weather12Hr = [...data.slice(timePoint, timePoint + 4)].map(
       (item) => {
         return item.prec_type;
       }
     );
-    return weather12Hr.includes("rain");
+    console.log(weather12Hr);
+    if (weather12Hr.length === 0 || currTime.getDate() - date.getDate() > 1) {
+      return true;
+    } else {
+      return weather12Hr.includes("rain");
+    }
   }
 
   useEffect(() => {
@@ -65,7 +84,12 @@ function Main() {
       <AstroCorner />
       <DisclaimerCorner />
       <ForecastCorner data={data} date={date} />
-      <Umbrella raining={checkRain()} data={data} date={date} />
+      <Umbrella
+        raining={checkRain()}
+        data={data}
+        date={date}
+        outdated={outdated}
+      />
     </>
   );
 }
