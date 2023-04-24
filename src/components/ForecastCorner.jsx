2ae1b3342aa63265308d6e2a63292b9ce84334a9
Tan Justinn <ohhoehoe@gmail.com>
2023-04-24 16:10:22 +0800

    Made elements look more clickable
    -Border appears around clickable elements to indicate to user that they can be clicked.

--- src/components/ForecastCorner.jsx
+++ src/components/ForecastCorner.jsx
@@ -5,7 +5,9 @@ import cornerStyle from "./Corner.module.css";
 function ForecastCorner(props) {
   return (
     <NavLink to="/forecast" state={{ data: props.data, date: props.date }}>
-      <img src="/umbrella.ico" className={cornerStyle.botRight} />
+      <div className={`${cornerStyle.botRight} ${cornerStyle.click}`}>
+        Detailed Forecast
+      </div>
     </NavLink>
   );
 }
